22487614746facb98db714e8dc8ad375b3b86fdd
Barak Rosner <rosnerbb@gmail.com>
2021-02-25 14:20:29 -0500

    commit tree 1958609be97bbb6ac5b1cb9aa275bc841e9f1b5c

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,17 @@
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight 
+
+    def initialize (species, weight, nickname)
+        @species = species 
+        @weight = weight
+        @nickname = nickname
+    end
+
+    def animal.all 
+        @animal = animal 
+    end
+
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,25 @@
 class Zoo
 
+    attr_accessor :name, :location
+    
+    @@all = []
+
+    def initialize (name, location)
+        @name = name
+        @location = location 
+        @@all << self 
+    end
+
+    def @animals = (animals)
+        @animals = animals
+    end
+
+
+    def self.all
+        @@all 
+        
+    end
+
+
+
 end
